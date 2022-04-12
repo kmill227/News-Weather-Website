398d70ce81931a96b11e834988df6e540e6ccb0d
Robin <mostost96@gmail.com>
2022-04-12 15:42:04 -0400

    Project initialization

---
+++ header.php
@@ -0,0 +1,37 @@
+<link href="bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+<div class="row">
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+              <div class="container-fluid">
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                      <div class="col-md-8">
+                          <ul class="navbar-nav">
+                          <li class="nav-item">
+                              <a class="nav-link active" aria-current="page" href="#">Home</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="#">Local News</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="#">Weather</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="#">Edit Account</a>
+                          </li>
+                          </ul>
+                      </div>
+                      <div class="col-md-4">
+                          <form class="d-flex">
+                              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                              <button class="btn btn-outline-light" type="submit">Search</button>
+                          </form>
+                      </div>
+                  </div>
+            </nav>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Global News</title>
+    <link href="bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"  rel="stylesheet" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"/>
+  </head>
+  <body>
+      <?php require_once "header.php" ?>
+      <main>
+        <div class="row">
+          <div class="col-md-8">
+            <div class = "row">
+                <div class="col-md-12">
+                    <img>
+                    <p>Lorem ipsum</p>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12">
+                    <table>
+                    <tr>
+                        <td>
+
+                        </td>
+                        <td>
+
+                        </td>
+                        <td>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+
+                        </td>
+                        <td>
+
+                        </td>
+                        <td>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+
+                        </td>
+                        <td>
+
+                        </td>
+                        <td>
+                            
+                        </td>
+                    </tr>
+                    </table>
+                </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+          </div>
+        </div>
+      </main>
+	<script src="index.js"></script>
+  </body>
+</html>
