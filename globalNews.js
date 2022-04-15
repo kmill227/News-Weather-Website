498374a8a304dec5de8e2ce419af354f03466928
Robin <mostost96@gmail.com>
2022-04-15 17:59:31 -0400

    Update Formatting

---
+++ globalNews.js
@@ -0,0 +1,55 @@
+async function fetchArticles(){
+    var url = 'https://newsapi.org/v2/top-headlines?' +'country=us&' + 'apiKey=653a1affea5f4084a09d20d4863a0a0c';
+
+    return await fetch(url).then(function(response){
+        return response.json();
+    }).then(function(json){
+        return json;
+    });
+}
+
+fetchArticles().then(function(result){
+    console.log(result.articles);
+    for(let i = 0; i < 10; i++){
+        if(i == 0){
+            var img = document.createElement('img');
+            img.src = result.articles[0].urlToImage;
+            var link = document.createElement("a");
+            link.href = result.articles[0].url;
+            var link2 = document.createElement("a");
+            link2.href = result.articles[0].url;
+
+            const title = document.createElement("h3");
+            const node = document.createTextNode(result.articles[0].title);
+            title.appendChild(node);
+
+            const element = document.getElementById("featured");
+            link.appendChild(img);
+            link.appendChild(title);
+            element.appendChild(link);
+
+            const desc = document.createElement("h4");
+            const node2 = document.createTextNode(result.articles[0].description);
+            const element2 = document.getElementById("featuredText");
+            desc.appendChild(node2);
+            link2.appendChild(title);
+            element2.appendChild(link2);
+            element2.appendChild(desc);
+        }
+        else{
+            var img = document.createElement('img');
+            img.src = result.articles[i].urlToImage;
+            var link = document.createElement("a");
+            link.href = result.articles[i].url;
+
+            const title = document.createElement("h6");
+            const node = document.createTextNode(result.articles[i].title);
+            title.appendChild(node);
+
+            const element = document.getElementById("article" + i);
+            link.appendChild(img);
+            link.appendChild(title);
+            element.appendChild(link);
+        }
+    }
+})
--- index.php
+++ index.php
@@ -6,64 +6,71 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Global News</title>
     <link href="bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="styles.css" rel=stylesheet>
     <link href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"  rel="stylesheet" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"/>
+    <script type="module" src="globalNews.js"></script>
   </head>
   <body>
       <?php require_once "header.php" ?>
       <main>
         <div class="row">
-          <div class="col-md-8">
-            <div class = "row">
-                <div class="col-md-12">
-                    <img>
-                    <p>Lorem ipsum</p>
-                </div>
+          <div class="col-md-9" id="newsCol">
+            <div class = "row" id="featuredRow">
+            <h1 id="featuredStoryTitle">Featured Story</h1>
+              <div class="col-md-8">
+                  <div id="featured">
+                  </div>
+              </div>
+              <div class="col-md-4">
+                <div id="featuredText"></div>
+              </div>
             </div>
             <hr>
+            <h2 id="moreStories">More Stories</h2>
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-12" id="storyCol">
                     <table>
                     <tr>
                         <td>
-
+                          <div id="article1"></div>
                         </td>
                         <td>
-
+                          <div id="article2"></div>
                         </td>
                         <td>
-
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-
-                        </td>
-                        <td>
-
-                        </td>
-                        <td>
-
+                          <div id="article3"></div>
                         </td>
                     </tr>
                     <tr>
                         <td>
-
+                          <div id="article4"></div>
                         </td>
                         <td>
-
+                          <div id="article5"></div>
                         </td>
                         <td>
-                            
+                          <div id="article6"></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                          <div id="article7"></div>
+                        </td>
+                        <td>
+                          <div id="article8"></div>
+                        </td>
+                        <td>
+                          <div id="article9"></div>
                         </td>
                     </tr>
                     </table>
                 </div>
             </div>
           </div>
-          <div class="col-md-4">
+          <div class="col-md-3" id="trendsCol">
+            <h2 id="trendsTitle">Trends</h2>
           </div>
         </div>
       </main>
-	<script src="index.js"></script>
   </body>
 </html>
