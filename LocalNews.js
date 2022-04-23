5555be055bd10c345d367972e7fa419866143d81
kmill227 <kmill227@kent.edu>
2022-04-23 15:39:02 -0400

    Create Local News Page, get api results

---
+++ LocalNews.js
@@ -0,0 +1,54 @@
+async function getLocalNews(location)
+{
+    const key = '263377734fef4966a4d70c607b678633';
+    var url = 'https://newsapi.org/v2/everything?q=' + '\"' + location + '\"' + '&sortBy=relevancy&apiKey=' + key;
+    return await fetch(url).then(response => response.json()).then(json => json);
+}
+
+getLocalNews("North Canton, Ohio").then(result =>
+{
+    for(var i = 0; i < 12; i++) // display first twelve results
+    {
+        var cityDiv = document.getElementById('City');
+        var div = document.createElement('div');
+        div.id = 'result' + i;
+        var a = document.createElement('a'); 
+        var head = document.createElement('h2');
+        a.href = result.articles[i].url;
+        var img = document.createElement('img');
+        img.src = result.articles[i].urlToImage;
+        head.innerHTML = result.articles[i].title;
+
+
+        a.appendChild(head); // hyperlink title of article and image 
+        a.appendChild(img);
+        div.appendChild(a);
+        cityDiv.appendChild(div); // append div to city div
+    }
+})
+
+getLocalNews("Ohio").then(result =>
+{
+    for(var i = 0; i < 12; i++) // display first twelve results 
+    {
+        var stateDiv = document.getElementById('State');
+        var div = document.createElement('div');
+        div.id = 'result' + i;
+ 
+        var head = document.createElement('h2');
+        head.innerHTML = result.articles[i].title;
+
+        var a = document.createElement('a'); 
+        a.href = result.articles[i].url;
+
+        var img = document.createElement('img');
+        img.src = result.articles[i].urlToImage;
+
+
+
+        a.appendChild(head); // hyperlink title of article and image 
+        a.appendChild(img);
+        div.appendChild(a);
+        stateDiv.appendChild(div); // append div to state div
+    }
+})
---
+++ LocalNews.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <script src='LocalNews.js'></script>
+        <title>
+            Local News
+        </title>
+    </head>
+    <body>
+        <main>
+            <div id='City'>
+                <h1 id = 'City News'>
+                    City News
+                </h1>
+            </div>
+            <div id='State'>
+                <h1 id='State News'>
+                    State News
+                </h1>
+            </div>
+        </main>
+    </body>
+</html>
