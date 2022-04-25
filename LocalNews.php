a8ef9226dcea6e882e43eb4f19863c2b691f1b2e
Robin <mostost96@gmail.com>
2022-04-25 12:32:45 -0400

    Fix header in localNews

--- LocalNews.php
+++ LocalNews.php
@@ -13,41 +13,7 @@
     </head>
     <body>
         <div class="row">
-        <div class="container-fluid">
-            <div class="col-md-12">
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                    <div class="container-fluid">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <div class="col-md-8">
-                                <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Local News</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Weather</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Edit Account</a>
-                                </li>
-                                </ul>
-                            </div>
-                            <div class="col-md-4" id="searchForm">
-                                <form class="d-flex" method="post" action="http://localhost/HCIProject/globalSearch.php">
-                                    <input class="form-control me-2" type="search" placeholder="Search" name="globalSearch" aria-label="Search">
-
-                                    <button class="btn btn-outline-light" type="submit">Search</button>
-                                </form>
-                            </div>
-                        </div>
-                </nav>
-            </div>
-        </div>
+        <?php require_once "header.php" ?>
         </div>
         <main>
             <div class="container mt-5" id='City'>
