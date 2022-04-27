1993dab32736ed84923eda2eceff36816fece941
kmill227 <kmill227@kent.edu>
2022-04-26 20:16:03 -0400

    Fix sql syntax error

--- db/hciProject.sql
+++ db/hciProject.sql
@@ -5,5 +5,5 @@ DROP TABLE IF EXISTS `globaltrends`;
 CREATE TABLE IF NOT EXISTS `globaltrends` (
   `GlobalTrend` text NOT NULL,
   `TotalSearches` int(11) DEFAULT NULL,
-  'LatestTime' datetime DEFAULT NULL
+  `LatestTime` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
